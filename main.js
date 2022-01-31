f612ed4b9db9be8a97c3c58169cc966bc83a649b
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2022-01-31 09:19:39 -0300

    add a plane

--- main.js
+++ main.js
@@ -5,17 +5,23 @@ function init(){
 
    const camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 0.1, 1000)
 
-   //mudando a posição da câmera
+   //mMdando a posição da câmera
    camera.position.x = 1
    camera.position.y = 2
    camera.position.z = 5
-   camera.lookAt(new THREE.Vector3(0, 0, 0)) //a câmera irá olhar para o centro do espaço 3D
+   camera.lookAt(new THREE.Vector3(0, 0, 0)) //A câmera irá olhar para o centro do espaço 3D
 
    const renderer = new THREE.WebGLRenderer()
    renderer.setSize(window.innerWidth, window.innerHeight)
 
-   const box = getBox(1, 1, 1)
+   //Add objetos geométricos
+   const box = getBox(1, 5, 1)
+   const plane = getPlane(4, 4)
+   plane.rotation.x = Math.PI / 2 //THREE.js usa radianos ao invés de graus
+   box.position.y = box.geometry.parameters.height / 2 //O box ficará em cima do plano
+
    scene.add(box)
+   scene.add(plane)
 
    document.getElementById('root').appendChild(renderer.domElement)
    renderer.render(scene, camera)
@@ -24,6 +30,14 @@ function init(){
 function getBox(w, h, d){
    const geometry = new THREE.BoxGeometry(w, h, d)
    const material = new THREE.MeshBasicMaterial({color: 'blue'})
+   const mesh = new THREE.Mesh(geometry, material)
+
+   return mesh
+}
+
+function getPlane(w, h){
+   const geometry = new THREE.PlaneGeometry(w, h)
+   const material = new THREE.MeshBasicMaterial({color: 'red', side: THREE.DoubleSide})
    const mesh = new THREE.Mesh(geometry, material)
 
    return mesh
